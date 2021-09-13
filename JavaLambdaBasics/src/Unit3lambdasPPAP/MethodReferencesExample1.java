453375ac9022c01df3b8eb22e346be3ef7c14101
581515 <47955690+Y581515@users.noreply.github.com>
2021-09-13 10:46:27 +0200

    vommit

---
+++ JavaLambdaBasics/src/Unit3lambdasPPAP/MethodReferencesExample1.java
@@ -0,0 +1,15 @@
+package Unit3lambdasPPAP;
+
+public class MethodReferencesExample1 {
+	public static void main(String[] args) {
+
+		Thread t = new Thread(() -> printMessage());
+		t.start();
+
+	}
+
+	public static void printMessage() {
+		System.out.println("Hello");
+	}
+
+}
