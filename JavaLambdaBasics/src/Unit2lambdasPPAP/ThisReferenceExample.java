2fc589b338de6637f5413ec3bdf4ba54b462bd77
581515 <47955690+Y581515@users.noreply.github.com>
2021-09-13 09:19:20 +0200

    20

---
+++ JavaLambdaBasics/src/Unit2lambdasPPAP/ThisReferenceExample.java
@@ -0,0 +1,32 @@
+package Unit2lambdasPPAP;
+
+import java.io.PrintStream;
+
+public class ThisReferenceExample {
+
+	public void doProcess(int i, Process p) {
+		p.process(i);
+	}
+
+	public void execute() {
+		doProcess(10, (i) -> {
+			System.out.println("Value of i is " + i);
+			System.out.println(this);
+		});
+
+	}
+
+	public static void main(String[] args) {
+
+		ThisReferenceExample thisReferenceExample = new ThisReferenceExample();
+
+//		thisReferenceExample.doProcess(10, (i) -> {
+//			System.out.println("Value of i is " + i);
+////			System.out.println(this);
+//		});
+
+		System.out.println(thisReferenceExample);
+		thisReferenceExample.execute();
+	}
+
+}
